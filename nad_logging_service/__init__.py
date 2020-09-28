8cb1e91e5221d582e880879060db4a704348cf71
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-09-28 00:19:12 -0400

    Cleanup.

--- nad_logging_service/__init__.py
+++ nad_logging_service/__init__.py
@@ -26,12 +26,6 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     # register routes
     app.register_blueprint(logger.bp)
 
