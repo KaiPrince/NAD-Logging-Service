e03782e1c07aba4f4d1bb72857c5ae5bce9abb66
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2020-09-30 09:42:21 -0400

    Add sample view for handling JSON data.

--- nad_logging_service/logger.py
+++ nad_logging_service/logger.py
@@ -1,7 +1,7 @@
 # TODO FHC
 # This is the logging service.
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 bp = Blueprint("logger", __name__)
 
@@ -9,3 +9,12 @@ bp = Blueprint("logger", __name__)
 @bp.route("/")
 def index():
     return "Hello World!"
+
+
+@bp.route("/log", methods=["GET", "POST"])
+def log():
+    """ This is a sample view that echos posted JSON. """
+    if request.method == "POST":
+        return request.json
+
+    return ""
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,4 +1,6 @@
 # TODO FHC
+import pytest
+
 
 def test_index_ok(client):
     """ Logger responds to a GET request. """
@@ -9,3 +11,16 @@ def test_index_ok(client):
 
     # Assert
     assert response.status_code == 200
+
+
+def test_logger_echo(client):
+    """ Logger echos a POST request. """
+    # Arrange
+    data = {"key": "value"}
+
+    # Act
+    response = client.post("/log", content_type="application/json", json=data)
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json == data
